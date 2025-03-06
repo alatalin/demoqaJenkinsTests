f3d88db7cc5f1ca92ee5f8b77d47204d67c9954f
AlexLatalin <a.latalin@myrambler.ru>
2025-03-06 13:38:02 +0300

    Added Attach

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -10,7 +10,7 @@ public class CalendarComponent {
         $(".react-datepicker__month-select").click();
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
-        $$(".react-datepicker__day").findBy(text(day)).click();
+        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
     }
 
 }
--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,9 +1,12 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.StudentRegistrationPage;
 import utils.TestDataFaker;
+
+import static io.qameta.allure.Allure.step;
 
 @Tag("demoqa")
 public class PracticeFormTests extends TestBase {
@@ -12,63 +15,81 @@ public class PracticeFormTests extends TestBase {
     TestDataFaker testDataFaker = new TestDataFaker();
 
     @Test
+    @DisplayName("Полное заполнение формы")
     public void succesfullFillAllPracticeFormTest() {
-        studentRegistrationPage.openPage()
-                .disableBanners()
-                .setFirstName(testDataFaker.firstName)
-                .setLastName(testDataFaker.lastName)
-                .setUserEmail(testDataFaker.email)
-                .setGenderWrapper(testDataFaker.gender)
-                .setUserNumber(testDataFaker.mobileNumber)
-                .setDateOfBirth(testDataFaker.dayOfBirth, testDataFaker.monthOfBirth, testDataFaker.yearOfBirth)
-                .setSubjects(testDataFaker.subject)
-                .setHobbies(testDataFaker.hobby)
-                .imageUpload(testDataFaker.picture)
-                .setCurrentAddress(testDataFaker.address)
-                .setUserState(testDataFaker.state)
-                .setUserCity(testDataFaker.city)
-                .submitButtonClick()
-
-                .checkRegistrationResultMessage("Thanks for submitting the form")
-                .checkRegistrationResultTable("Student Name",testDataFaker.firstName + " " + testDataFaker.lastName)
-                .checkRegistrationResultTable("Student Email",testDataFaker.email)
-                .checkRegistrationResultTable("Gender",testDataFaker.gender)
-                .checkRegistrationResultTable("Mobile",testDataFaker.mobileNumber)
-                .checkRegistrationResultTable("Date of Birth",testDataFaker.dayOfBirth + " " + testDataFaker.monthOfBirth + "," + testDataFaker.yearOfBirth)
-                .checkRegistrationResultTable("Subjects",testDataFaker.subject)
-                .checkRegistrationResultTable("Hobbies",testDataFaker.hobby)
-                .checkRegistrationResultTable("Picture",testDataFaker.picture)
-                .checkRegistrationResultTable("Address",testDataFaker.address)
-                .checkRegistrationResultTable(testDataFaker.state,testDataFaker.city);
+        step("Открываем форму", () -> {
+            studentRegistrationPage.openPage()
+                .disableBanners();
+        });
+        step("Заполняем форму", () -> {
+            studentRegistrationPage.setFirstName(testDataFaker.firstName)
+                    .setLastName(testDataFaker.lastName)
+                    .setUserEmail(testDataFaker.email)
+                    .setGenderWrapper(testDataFaker.gender)
+                    .setUserNumber(testDataFaker.mobileNumber)
+                    .setDateOfBirth(testDataFaker.dayOfBirth, testDataFaker.monthOfBirth, testDataFaker.yearOfBirth)
+                    .setSubjects(testDataFaker.subject)
+                    .setHobbies(testDataFaker.hobby)
+                    .imageUpload(testDataFaker.picture)
+                    .setCurrentAddress(testDataFaker.address)
+                    .setUserState(testDataFaker.state)
+                    .setUserCity(testDataFaker.city)
+                    .submitButtonClick();
+        });
+        step("Проверяем результат", () -> {
+            studentRegistrationPage.checkRegistrationResultMessage("Thanks for submitting the form")
+                    .checkRegistrationResultTable("Student Name", testDataFaker.firstName + " " + testDataFaker.lastName)
+                    .checkRegistrationResultTable("Student Email", testDataFaker.email)
+                    .checkRegistrationResultTable("Gender", testDataFaker.gender)
+                    .checkRegistrationResultTable("Mobile", testDataFaker.mobileNumber)
+                    .checkRegistrationResultTable("Date of Birth", testDataFaker.dayOfBirth + " " + testDataFaker.monthOfBirth + "," + testDataFaker.yearOfBirth)
+                    .checkRegistrationResultTable("Subjects", testDataFaker.subject)
+                    .checkRegistrationResultTable("Hobbies", testDataFaker.hobby)
+                    .checkRegistrationResultTable("Picture", testDataFaker.picture)
+                    .checkRegistrationResultTable("Address", testDataFaker.address)
+                    .checkRegistrationResultTable(testDataFaker.state, testDataFaker.city);
+        });
     }
 
     @Test
+    @DisplayName("Минимальное заполнение формы")
     public void succesfullMimimumDataPracticeFormTest() {
-        studentRegistrationPage.openPage()
-                .disableBanners()
-                .setFirstName(testDataFaker.firstName)
-                .setLastName(testDataFaker.lastName)
-                .setGenderWrapper(testDataFaker.gender)
-                .setUserNumber(testDataFaker.mobileNumber)
-                .submitButtonClick()
-
-                .checkRegistrationResultMessage("Thanks for submitting the form")
-                .checkRegistrationResultTable("Student Name",testDataFaker.firstName + " " + testDataFaker.lastName)
-                .checkRegistrationResultTable("Gender",testDataFaker.gender)
-                .checkRegistrationResultTable("Mobile",testDataFaker.mobileNumber);
+        step("Открываем форму", () -> {
+            studentRegistrationPage.openPage()
+                    .disableBanners();
+        });
+        step("Заполняем форму", () -> {
+            studentRegistrationPage.setFirstName(testDataFaker.firstName)
+                    .setLastName(testDataFaker.lastName)
+                    .setGenderWrapper(testDataFaker.gender)
+                    .setUserNumber(testDataFaker.mobileNumber)
+                    .submitButtonClick();
+        });
+        step("Проверяем результат", () -> {
+            studentRegistrationPage.checkRegistrationResultMessage("Thanks for submitting the form")
+                    .checkRegistrationResultTable("Student Name", testDataFaker.firstName + " " + testDataFaker.lastName)
+                    .checkRegistrationResultTable("Gender", testDataFaker.gender)
+                    .checkRegistrationResultTable("Mobile", testDataFaker.mobileNumber);
+        });
     }
 
     @Test
+    @DisplayName("Невалидный номер телефона")
     public void notValidPhoneNumberPracticeFormTest() {
-        studentRegistrationPage.openPage()
-                .disableBanners()
-                .setFirstName(testDataFaker.firstName)
-                .setLastName(testDataFaker.lastName)
-                .setGenderWrapper(testDataFaker.gender)
-                .setUserNumber(testDataFaker.notValidMobileNumber)
-                .submitButtonClick()
-
-                .checkMobileNumberError();
+        step("Открываем форму", () -> {
+            studentRegistrationPage.openPage()
+                    .disableBanners();
+        });
+        step("Заполняем форму", () -> {
+            studentRegistrationPage.setFirstName(testDataFaker.firstName)
+                    .setLastName(testDataFaker.lastName)
+                    .setGenderWrapper(testDataFaker.gender)
+                    .setUserNumber(testDataFaker.notValidMobileNumber)
+                    .submitButtonClick();
+        });
+        step("Проверяем результат", () -> {
+            studentRegistrationPage.checkMobileNumberError();
+        });
     }
 
 }
